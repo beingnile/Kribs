b45a7e18dce552c7adf2d51a9bc8196639bd9712
Nile Okomo <okomonile3@gmail.com>
2024-06-11 18:00:29 +0300

    Fix linter errors

--- kribs-backend/config/db.js
+++ kribs-backend/config/db.js
@@ -4,9 +4,9 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true
     });
-    console.log("Database connected!");
+    console.log('Database connected!');
   } catch (err) {
     console.error(err.message);
     process.exit(1);
